ee41aea6c59315a8f65f7b459641b3f3e82ffa31
ChoiHyoSik <huchoi24@naver.com>
2022-08-31 15:16:56 +0900

    Add buildSrc module

---
+++ buildSrc/src/main/java/kr/co/buildsrc/Dependencies.kt
@@ -0,0 +1,75 @@
+package kr.co.buildsrc
+
+object Versions {
+    const val KOTLIN_VERSION = "1.7.10"
+    const val KOTLINX_COROUTINES = "1.6.1"
+    const val KOTLINX_COROUTINES_TEST = "1.6.2"
+    const val BUILD_GRADLE = "7.2.1"
+    const val COMPILE_SDK_VERSION = 32
+    const val BUILD_TOOLS_VERSION = "30.0.3"
+    const val MIN_SDK_VERSION = 24
+    const val TARGET_SDK_VERSION = 32
+    const val CONSTRAINTLAYOUT = "2.1.4"
+    const val SWIPEREFRESHLAYOUT = "1.1.0"
+    const val CORE_KTX = "1.7.0"
+    const val APP_COMPAT = "1.4.2"
+    const val ACTIVITY_KTX = "1.2.3"
+    const val FRAGMENT_KTX = "1.3.4"
+    const val LIFECYCLE_KTX = "2.3.1"
+    const val HILT = "2.42"
+    const val MATERIAL = "1.6.1"
+    const val RETROFIT = "2.8.2"
+    const val OKHTTP = "4.9.0"
+    const val JUNIT = "4.13.2"
+    const val ANDROID_JUNIT = "1.1.3"
+    const val ESPRESSO_CORE = "3.4.0"
+    const val TRUTH = "1.0.1"
+    const val ROOM = "2.4.3"
+}
+
+object Kotlin {
+    const val KOTLIN_STDLIB = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.KOTLIN_VERSION}"
+    const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.KOTLINX_COROUTINES}"
+    const val COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.KOTLINX_COROUTINES}"
+}
+
+object AndroidX {
+    const val CORE_KTX = "androidx.core:core-ktx:${Versions.CORE_KTX}"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:${Versions.APP_COMPAT}"
+    const val CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:${Versions.CONSTRAINTLAYOUT}"
+    const val SWIPEREFRESHLAYOUT = "androidx.swiperefreshlayout:swiperefreshlayout:${Versions.SWIPEREFRESHLAYOUT}"
+    const val ACTIVITY_KTX = "androidx.activity:activity-ktx:${Versions.ACTIVITY_KTX}"
+    const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:${Versions.FRAGMENT_KTX}"
+    const val LIFECYCLE_VIEWMODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.LIFECYCLE_KTX}"
+    const val LIFECYCLE_LIVEDATA_KTX = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.LIFECYCLE_KTX}"
+    const val ROOM_RUNTIME = "androidx.room:room-runtime:${Versions.ROOM}"
+    const val ROOM_COMPILE = "androidx.room:room-compiler:${Versions.ROOM}"
+    const val ROOM_KTX = "androidx.room:room-ktx:${Versions.ROOM}"
+}
+
+object Google {
+    const val HILT_ANDROID = "com.google.dagger:hilt-android:${Versions.HILT}"
+    const val HILT_ANDROID_COMPILER = "com.google.dagger:hilt-android-compiler:${Versions.HILT}"
+    const val MATERIAL = "com.google.android.material:material:${Versions.MATERIAL}"
+}
+
+object Libraries {
+    const val RETROFIT = "com.squareup.retrofit2:retrofit:${Versions.RETROFIT}"
+    const val RETROFIT_CONVERTER_GSON = "com.squareup.retrofit2:converter-gson:${Versions.RETROFIT}"
+    const val OKHTTP = "com.squareup.okhttp3:okhttp:${Versions.OKHTTP}"
+    const val OKHTTP_LOGGING_INTERCEPTOR = "com.squareup.okhttp3:logging-interceptor:${Versions.OKHTTP}"
+}
+
+object UnitTest {
+    const val JUNIT = "junit:junit:${Versions.JUNIT}"
+    const val MOCK_WEB_SERVER = "com.squareup.okhttp3:mockwebserver:${Versions.OKHTTP}"
+    const val TRUTH = "com.google.truth:truth:${Versions.TRUTH}"
+    const val COROUTINE_TEST = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.KOTLINX_COROUTINES_TEST}"
+}
+
+object AndroidTest {
+    const val ANDROID_JUNIT = "androidx.test.ext:junit:${Versions.ANDROID_JUNIT}"
+    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${Versions.ESPRESSO_CORE}"
+    const val ANDROID_TRUTH = "com.google.truth:truth:${Versions.TRUTH}"
+    const val ANDROID_COROUTINE_TEST = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.KOTLINX_COROUTINES_TEST}"
+}
